9245b4ee7ecf9bab40bba85724d4aed8fd75230e
isaac <shifdub@gmail.com>
2020-10-22 11:12:04 -0600

    parsing from stdIn writing to stdOut.

--- src/edu/cnm/deepdive/Parser.java
+++ src/edu/cnm/deepdive/Parser.java
@@ -8,17 +8,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.stream.Collectors;
 
 public class Parser {
 
-  private static final String TEST_SCHEMA_FILE = "test-schema.json";
+  private static final String STATEMENT_TERMINATOR = ";";
+  private static final String STATEMENT_SEPARATOR = String.format("%s%n%n", STATEMENT_TERMINATOR);
+  private Schema schema;
 
   public static void main(String... args) throws IOException {
-    try (InputStream input = new FileInputStream(TEST_SCHEMA_FILE)) {
-      Parser parser = new Parser();
-      Schema schema = parser.parse(input);
-      System.out.println(parser.getDdl(schema));
-    }
+    Parser parser = new Parser();
+    Schema schema = parser.parse(System.in);
+    System.out.println(parser.getDdl(schema));
   }
 
   private Schema parse(InputStream input) throws IOException {
@@ -31,8 +32,9 @@ public class Parser {
   }
 
   private String getDdl(Schema schema) {
-    //TODO Assemble all ddl from objects read by Gson
-    return null;
+    this.schema = schema;
+    return schema.getDdl()
+        .collect(Collectors.joining(STATEMENT_SEPARATOR, "", STATEMENT_TERMINATOR));
   }
 
 }
--- src/edu/cnm/deepdive/model/Schema.java
+++ src/edu/cnm/deepdive/model/Schema.java
@@ -1,9 +1,13 @@
 package edu.cnm.deepdive.model;
 
+import com.google.gson.annotations.Expose;
+import java.util.stream.Stream;
 import javax.xml.crypto.Data;
 
 public class Schema {
 
+  private static final String PLACEHOLDER = "${TABLE_NAME}";
+  @Expose
   private Database database;
 
   public Database getDatabase() {
@@ -12,5 +16,16 @@ public class Schema {
 
   public void setDatabase(Database database) {
     this.database = database;
+  }
+  public Stream<String> getDdl() {
+    return database.getEntities().stream()
+      .flatMap((entity) ->
+          Stream.concat(
+              Stream.of(entity.getDdl()),
+              entity.getIndices().stream()
+                  .map(Index::getDdl)
+            )
+          .map((s)  -> s.replace(PLACEHOLDER, entity.getName()))
+      );
   }
 }
